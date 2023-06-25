bed22d6762bcedaea4e006807f664ce1b502e720
ypk-143 <yaswanthganji123@gmail.com>
2023-06-25 18:14:31 +0530

    aws final commit

--- cmail.py
+++ cmail.py
@@ -2,9 +2,9 @@ import smtplib
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('yaswanthganji123@gmail.com','fsfzeiwgzcjoswzo')
+    server.login('ytsofficial111@gmail.com','phoqgxytashtakjj')
     msg=EmailMessage()
-    msg['From']='yaswanthganji23@gmail.com'
+    msg['From']='ytsofficial111@gmail.com'
     msg['To']=to
     msg['Subject']=subject
     msg.set_content(body)
